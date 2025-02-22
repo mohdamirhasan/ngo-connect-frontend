7baed0849bacc834e14a0ecdd1838c2f753e085c
Haseen Fatima <haseenf2805@gmail.com>
2025-02-22 22:01:34 +0530

    login user

---
+++ src/pages/LoginUser.tsx
@@ -0,0 +1,116 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const formSchema = z.object({
+	email: z.string().email("*Invalid email").nonempty("*This field is required"),
+	password: z.string().min(6, "*Password must be atleast of 6 characters"),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+const LoginUser = () => {
+
+	const navigate = useNavigate();
+
+	const form = useForm<FormValues>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: '',
+			password: '',
+		}
+	})
+
+	const onSubmit = async (data: FormValues) => {
+		try{
+			const response = await fetch("http://localhost:5001/api/users/login", {
+			  method: 'POST',
+			  headers: {
+				'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify(data),
+			});
+	  
+			const result = await response.json();
+			if(response.ok){
+			  alert(result.message);
+			  navigate('/');
+			}
+			else{
+			  alert("Error: " + result.message);
+			}
+		  }
+		  catch (error) {
+			alert("An error occured, Please try again!")
+		  }
+	}
+
+	return (
+		<>
+		<Navbar />
+		<div className="max-w-md mx-auto p-4 border rounded-lg shadow mt-16">
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="p-5 space-y-5">
+					<h1 className="font-bold text-2xl pb-4">Login to your account</h1>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+						<FormItem className="text-left w-full">
+							<FormLabel className="m-2">Email</FormLabel>
+							<FormControl>
+							<Input
+								type="email"
+								placeholder="Enter Email address"
+								{...field}
+							/>
+							</FormControl>
+							<FormMessage className="text-xs"/>
+						</FormItem>
+						)}
+					/>
+					 <FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+						<FormItem className="text-left w-full">
+							<FormLabel className="m-2">Password</FormLabel>
+							<FormControl>
+							<Input
+								type="password"
+								placeholder="Enter password"
+								{...field}
+							/>
+							</FormControl>
+							<FormMessage className="text-xs"/>
+						</FormItem>
+						)}
+					/>
+					<div className="flex flex-col justify-center p-2 gap-4">
+						<Button type="submit" className="w-full">
+						Sign in
+						</Button>
+						<Button className="text-sm font-light invert" onClick={() => navigate('/register-user')}>New user? Register</Button>
+					</div>
+				</form>
+			</Form>
+		</div>
+		<Footer />
+		</>
+	)
+}
+
+export default LoginUser
